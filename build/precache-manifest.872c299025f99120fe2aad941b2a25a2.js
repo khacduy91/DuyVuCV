c6f5e24ac1c85cad8dbcbbff5443209c1bc250c5
Assist2709 <khacduyland2019@gmail.com>
2020-04-25 12:21:18 +0700

    3rd commit

---
+++ build/precache-manifest.872c299025f99120fe2aad941b2a25a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5855e424460a287627a3fff91dc25881",
+    "url": "/DuyVuCV/index.html"
+  },
+  {
+    "revision": "a1fd3ea52ee43541fa77",
+    "url": "/DuyVuCV/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9df037c7d143d5dbc9c9",
+    "url": "/DuyVuCV/static/css/main.fb3a9321.chunk.css"
+  },
+  {
+    "revision": "a1fd3ea52ee43541fa77",
+    "url": "/DuyVuCV/static/js/2.1401cf02.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/DuyVuCV/static/js/2.1401cf02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df037c7d143d5dbc9c9",
+    "url": "/DuyVuCV/static/js/main.db59d5d1.chunk.js"
+  },
+  {
+    "revision": "f6012049952ff9b9f805",
+    "url": "/DuyVuCV/static/js/runtime-main.4e7883dd.js"
+  }
+]);
